cd3e03fece4281bdf606802644a9fc87aa156e93
ArktinenSieni <matti.raty@helsinki.fi>
2016-05-17 17:00:11 +0300

    postgres connection

--- src/main/java/wepaht/configuration/HerokuConfiguration.java
+++ src/main/java/wepaht/configuration/HerokuConfiguration.java
@@ -22,6 +22,6 @@ public class HerokuConfiguration {
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        return new org.apache.tomcat.jdbc.pool.DataSource();
     }
 }
