92eced60d21126427712991e8e964694c05a1c99
ArktinenSieni <matti.raty@helsinki.fi>
2016-05-17 16:09:34 +0300

    postgres connection in heroku

--- src/main/java/wepaht/profile/ProdProfile.java
+++ src/main/java/wepaht/profile/ProdProfile.java
@@ -1,81 +1,21 @@
 package wepaht.profile;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import wepaht.domain.User;
-import wepaht.repository.UserRepository;
-
 import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile(value = "prod")
 public class ProdProfile {
-
-//    @Autowired
-//    private UserRepository userRepository;
-
-    @Bean
-    public PlatformTransactionManager transactionManager() throws URISyntaxException {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
     
     @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("org.postgresql.Driver");
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-        basicDataSource.setTestOnBorrow(true);
-        basicDataSource.setTestWhileIdle(true);
-        basicDataSource.setTestOnReturn(true);
-        basicDataSource.setValidationQuery("SELECT 1");
-
-        return basicDataSource;
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
     }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaDialect(new HibernateJpaDialect());
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPersistenceUnitName("production");
-        factory.setPackagesToScan("wepaht.domain");
-        factory.setDataSource(dataSource());
-
-        factory.afterPropertiesSet();
-        return factory;
-    }
-
-//    public void init() {
-//        if (userRepository.findAll().isEmpty()) {
-//            User admin = new User();
-//            admin.setRole("ADMIN");
-//            admin.setUsername("admin");
-//            admin.setPassword("admin");
-//            userRepository.save(admin);
-//        }
-//    }
 }
